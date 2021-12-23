6fb9cd692c466093d17041058b0ccf69ee26cdb1
Jaimin023 <79679723+Jaimin023@users.noreply.github.com>
2021-12-23 15:38:06 +1300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region maincode
+            int endpoint = 100;
+            for (int i=0; i<= endpoint; i++)
+            {
+                if(i%3==0 && i%5==0)
+                {
+                    Console.WriteLine("FooBar");            //number is divisible by 3 and 5, print ‘FooBar’
+                    
+                }
+                else if(i%5==0)
+                {
+                    Console.WriteLine("Bar");               //number is divisible by 5, print ‘Bar’
+
+                }
+                else if(i%3==0)
+                {
+                    Console.WriteLine("Foo");               // number is divisible by 3, print ‘Foo’
+
+                }
+                else
+                {
+                    Console.WriteLine(i);                   //number is not divisible by 3 or 5, print the number
+                }
+
+            }
+            #endregion
+        }
+    }
+}
